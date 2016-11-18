91420a8d8ce493c1b8083a3de0d5914d83d7e960
Chris Matthews <Christopher.Matthews@capitalone.com>
2016-11-18 13:32:12 +0000

    Added simple Hello World node app.

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+
+const app = express();
+app.use(bodyParser.json());
+
+// Routes
+app.get('/hello', (req, res, next) => {
+    res.status(200).json({message: 'Hello, World!'});
+});
+
+app.get('/hello/:name', (req, res, next) => {
+    const name = req.params.name;
+    res.status(200).json({message: `Hello, ${name}!`});
+});
+
+// Error Handlers
+app.use((req, res, next) => {
+    const err = new Error();
+    err.message = 'Not Found';
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        error: {err}
+    });
+});
+
+// Boot up HTTP server
+app.server = http.createServer(app);
+app.server.listen(3000);
+
+export default app;
