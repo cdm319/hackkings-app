1da216ecdb470634302b42817158959cfb7513de
Chris Matthews <Christopher.Matthews@capitalone.com>
2016-11-25 13:30:10 +0000

    Removed code to make base.

--- client/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IndexComponent from './components/IndexComponent';
-
-ReactDOM.render(<IndexComponent />, document.getElementById('app'));
